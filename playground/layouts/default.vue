a909b098cfd970b0f92178d0d8bc791e35487b59
frostycoles <dcfrostad@gmail.com>
2024-07-26 12:51:15 -0400

    feat(playground): Adds darkmode  option

--- playground/layouts/default.vue
+++ playground/layouts/default.vue
@@ -1,13 +1,41 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+const isDark = computed({
+  get() {
+    return colorMode.value === 'dark'
+  },
+  set() {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  },
+})
+</script>
+
 <template>
   <div class="w-full min-h-[100vh] m-0 fex flex-col">
-    <header class="bg-white w-full relative flex items-center text-lg h-16 px-8 shadow-md">
+    <header class="w-full relative flex items-center text-lg h-16 px-8 shadow-md">
       <UIcon
         name="i-fluent-emoji-high-contrast:bagel"
         class="w-5 h-5 mr-4"
       />
-      Nuxt Donate a Bagel!
+      Nuxt Donate a Bagel
+
+      <div class="flex-grow" />
+
+      <ClientOnly>
+        <UButton
+          :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+          color="gray"
+          variant="ghost"
+          aria-label="Theme"
+          @click="isDark = !isDark"
+        />
+        <template #fallback>
+          <div class="w-8 h-8" />
+        </template>
+      </ClientOnly>
     </header>
-    <UContainer class="bg-white max-w-[960px] w-full flex-grow p-4 pt-8">
+    <UContainer class="max-w-[960px] w-full flex-grow p-4 pt-8">
       <NuxtPage />
       <div class="mt-8 flex gap-8">
         <ULink
